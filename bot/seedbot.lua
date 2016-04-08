8b190988ef93076a5c94bf6b6b413d8dcdb00c3c
matin <matinsecret16@gmail.com>
2016-04-09 03:01:09 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {149223247},--Sudo users
+    sudo_users = {149223247,173792529},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
